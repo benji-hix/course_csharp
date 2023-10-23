2313cf1b56c3cfbcd782a6a8ef2e978f432e165e
Benji Hix <benji.hix@outlook.com>
2023-10-23 00:22:19 -0700

    first optional assignments

---
+++ intro/optional_fundamentals-1/Program.cs
@@ -0,0 +1,34 @@
+﻿//* Comment test
+
+//* 1)
+/*
+for (int i = 1; i <= 255; i++) {
+    Console.WriteLine($"{i}");
+}
+*/
+
+//* 2 + 3)
+/*
+Random rand = new Random();
+int sum = 0;
+
+for (int i = 1; i <= 5; i++) {
+    sum += rand.Next(10, 21);
+}
+
+Console.WriteLine($"sum of all numbers is {sum}");
+*/
+
+//* 4)
+
+for (int i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 != 0) {
+        Console.WriteLine("Fizz");
+    }
+    else if (i % 3 != 0 && i % 5 == 0) {
+        Console.WriteLine("Buzz");
+    }
+    else if (i % 3 == 0 && i % 5 == 0) {
+        Console.WriteLine("FizzBuzz");
+    }
+}
---
+++ intro/testing/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Random rand = new Random();
+
+for (int i = 1; i<= 10; i++) {
+    Console.WriteLine(rand.Next(11));
+}
